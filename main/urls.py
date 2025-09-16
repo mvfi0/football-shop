0089e2929ca1baa5129ae91336e0774f1fd86941
mvfi0 <muhammad.vegard@ui.ac.id>
2025-09-17 00:27:06 +0700

    Assignment 2 done.

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,25 @@
-# main/urls.py
 from django.urls import path
-from .views import identity
+from django.urls import path
+from .views import (
+    object_list, 
+    add_object, 
+    detail_object,
+    show_xml, 
+    show_json, 
+    show_xml_by_id, 
+    show_json_by_id
+)
+
+app_name = "main"
 
 urlpatterns = [
-    path('', identity, name='identity'),
+    path("", object_list, name="object_list"),
+    path("add/", add_object, name="add_object"),
+    path("detail/<int:obj_id>/", detail_object, name="detail_object"),
+
+    # New API endpoints
+    path("xml/", show_xml, name="show_xml"),
+    path("json/", show_json, name="show_json"),
+    path("xml/<int:obj_id>/", show_xml_by_id, name="show_xml_by_id"),
+    path("json/<int:obj_id>/", show_json_by_id, name="show_json_by_id"),
 ]
